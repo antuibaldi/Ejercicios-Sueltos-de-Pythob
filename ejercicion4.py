2290c88e330d415d714d1d0094a8e073adb145f0
Antu <122485403+antukung@users.noreply.github.com>
2024-03-13 00:33:40 -0300

    Ejercicio N4
    Trabajo practico

---
+++ ejercicion4.py
@@ -0,0 +1,7 @@
+numero_uno= int(input("Dame un numero"))
+numero_dos= int(input("Dame un segundo numero"))
+numero_tres= int(input("Dame un tercer numero"))
+
+resultado= (numero_uno+numero_dos)/numero_tres
+
+print("La respuesta es: "+(str(resultado)))
