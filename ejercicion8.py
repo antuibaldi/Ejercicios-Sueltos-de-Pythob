ae145d1d2e7d9093093f62d7ee412d3eccbada29
Antu <122485403+antukung@users.noreply.github.com>
2024-03-18 09:03:25 -0300

    Ejercicio Nº8
    Practica de programacion

---
+++ ejercicion8.py
@@ -0,0 +1,9 @@
+numero_uno = int(input("Dame un numero"))
+numero_dos = int(input("Dame un segundo numero"))
+
+i=0
+
+while i < 2:
+    numero_uno=numero_uno*numero_dos
+    print(numero_uno)
+    i=input("Dame un uno para seguir Dos para CERRAR") 
